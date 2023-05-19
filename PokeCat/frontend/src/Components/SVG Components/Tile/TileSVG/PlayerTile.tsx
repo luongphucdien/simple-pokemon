2737b85f8e449fc648f13c017f7b017744f8e3ad
luongphucdien <work.luongphucdien@gmail.com>
2023-05-19 15:35:38 +0700

    modify: Render online player with random coord

--- PokeCat/frontend/src/Components/SVG Components/Tile/TileSVG/PlayerTile.tsx
+++ PokeCat/frontend/src/Components/SVG Components/Tile/TileSVG/PlayerTile.tsx
@@ -5,10 +5,10 @@ export const PlayerTile = (props: TileProps) => {
         offsetX,offsetY
     } = props
     return(
-        <svg >
+        <g>
             <rect x={offsetX} y={offsetY} width="40" height="40" fill="green"/>
             {/* <line x1="0" x2="40" y1="0" y2="40" stroke="red"/>
             <line x1="40" x2="0" y1="0" y2="40" stroke="red"/> */}
-        </svg>
+        </g>
     )
 }
