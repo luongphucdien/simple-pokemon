7132e16a04bbb04d61d27b47d3e5a1eb749e94b8
luongphucdien <work.luongphucdien@gmail.com>
2023-05-23 19:11:23 +0700

    fix: Align player to center of map

--- PokeCat/frontend/src/API/index.tsx
+++ PokeCat/frontend/src/API/index.tsx
@@ -8,10 +8,7 @@ async function test(setState: (data:any)=>void) {
 }
 
 async function sendAction(action: string, username: string) {
-    await axios.post(`${API_URL}/player/action`, {action: action, username: username}).then((r) => {
-        console.log(r.data["world-data"])
-        // setWorldGrid(r.data["world-data"]["WorldGrid"])
-    }).catch((error: AxiosError) => {
+    await axios.post(`${API_URL}/player/action`, {action: action, username: username}).catch((error: AxiosError) => {
         console.log(error.response)
     })
     
@@ -23,10 +20,13 @@ async function testNoParams() {
     })
 }
 
-async function getWorld(setState: (data: any) => void) {
-    await axios.get(`${API_URL}/world`).then(r => {
-        console.log(r.data["world_data"])
-        setState(r.data.world)
+async function getWorld(username: string, setState: (data: any) => void, setPlayerCoord: (data: any) => void, map: SVGElement) {
+    await axios.get(`${API_URL}/world/${username}`).then(r => {
+        console.log(r.data["world-data"])
+        setState(r.data["world-data"]["WorldGrid"])
+        let playerCoord = r.data["playerCoord"]
+        setPlayerCoord(playerCoord)
+        map.setAttribute("transform", "translate(" + (180 - playerCoord[0]*40) + "," + (180 - playerCoord[1]*40) + ")")
     }).catch((error: AxiosError) => {
         console.log(error.response)
     })
